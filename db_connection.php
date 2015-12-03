1f5e0195988a55779fb1d98c70bae25349ff77ff
vojtechkrocan <kalivodav@gmail.com>
2015-12-04 00:25:58 +0100

    latin2

--- db_connection.php
+++ db_connection.php
@@ -4,4 +4,5 @@
 	$db = new mysqli($servername, $username, $password, $dbname);
 	if ($db->connect_error)
 		die("Nepodaøilo se pøipojit k databázi: " . $db->connect_error);
+	$db->set_charset('latin2')
 ?>
